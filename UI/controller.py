c6afcee9133f412b3646aaa11645a1115a14698d
Giuseppe Averta <g.averta3@gmail.com>
2024-05-05 15:28:57 +0200

    add some preliminary code

--- UI/controller.py
+++ UI/controller.py
@@ -8,8 +8,36 @@ class Controller:
         # the model, which implements the logic of the program and holds the data
         self._model = model
 
-    def handleCreaGrafo(self):
+    def handleCreaGrafo(self,e):
         pass
 
-    def loadFermate(self,e):
+    def handleCercaRaggiungibili(self):
         pass
+
+    def loadFermate(self, dd: ft.Dropdown()):
+        fermate = self._model.fermate
+
+        if dd.label == "Stazione di Partenza":
+            for f in fermate:
+                dd.options.append(ft.dropdown.Option(text=f.nome,
+                                                     data=f,
+                                                     on_click=self.read_DD_Partenza))
+        elif dd.label == "Stazione di Arrivo":
+            for f in fermate:
+                dd.options.append(ft.dropdown.Option(text=f.nome,
+                                                     data=f,
+                                                     on_click=self.read_DD_Arrivo))
+
+    def read_DD_Partenza(self,e):
+        print("read_DD_Partenza called ")
+        if e.control.data is None:
+            self._fermataPartenza = None
+        else:
+            self._fermataPartenza = e.control.data
+
+    def read_DD_Arrivo(self,e):
+        print("read_DD_Arrivo called ")
+        if e.control.data is None:
+            self._fermataArrivo = None
+        else:
+            self._fermataArrivo = e.control.data
--- UI/view.py
+++ UI/view.py
@@ -40,6 +40,8 @@ class View(ft.UserControl):
         self._btnCrea = ft.ElevatedButton(text="Crea Grafo", on_click=self._controller.handleCreaGrafo)
         self._ddStazPartenza = ft.Dropdown(label="Stazione di Partenza")
         self._ddStazArrivo = ft.Dropdown(label="Stazione di Arrivo")
+        self._btnCalcola = ft.ElevatedButton(text="Calcola Raggiungibili", on_click=self._controller.handleCercaRaggiungibili)
+
 
         #Load elements in DD
         self._controller.loadFermate(self._ddStazPartenza)
@@ -49,6 +51,7 @@ class View(ft.UserControl):
         row2 = ft.Row([self._btnCrea,
                        self._ddStazPartenza,
                        self._ddStazArrivo,
+                       self._btnCalcola,
                        ], alignment=ft.MainAxisAlignment.CENTER, spacing=30)
 
         # Row with listview
