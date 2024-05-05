3e2b5a35f22122a0bdd2d0f5ea6e0688eb4376bc
Giuseppe Averta <g.averta3@gmail.com>
2024-05-05 14:49:03 +0200

    startup code

---
+++ UI/controller.py
@@ -0,0 +1,21 @@
+import flet as ft
+
+
+class Controller:
+    def __init__(self, view, model):
+        # the view, with the graphical elements of the UI
+        self._view = view
+        # the model, which implements the logic of the program and holds the data
+        self._model = model
+
+    def handleCreaMulti(self):
+        pass
+
+    def handleCercaRaggiungibili(self):
+        pass
+
+    def handleCercaPercorso(self):
+        pass
+
+    def loadFermate(self,e):
+        pass
